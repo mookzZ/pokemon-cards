0d8e0295c1dad2cfdba4b0831d4ab0caa84588c3
acided <homejet2007@gmail.com>
2023-09-27 14:02:20 +0600

    сделал запрос на pokemon api, полученные данные выыел на страницу в виде карточек

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  const [pokemons, setPokemons] = useState([]);
+
+  const getPokemons = async () => {
+    const pokemonData = [];
+
+    for (let i = 1; i <= 21; i++) {
+      try {
+        const resp = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
+        const data = await resp.data;
+        pokemonData.push(data);
+      } catch (error) {
+        console.error("Ошибка: ", error);
+      }
+    }
+
+    setPokemons(pokemonData);
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <div>
+      <div className="wrapper">
+        <div className="pokemons">
+          {pokemons.map((pokemon) => (
+            <div key={pokemon.id} className={'pokemon'}>
+              <h2>{pokemon.name}</h2>
+              <img
+                src={pokemon.sprites.front_default}
+                alt={pokemon.name}
+                width="100"
+                height="100"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
